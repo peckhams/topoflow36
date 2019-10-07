ddc0320b2a9339c809033d6f83cc2b329febffb9
Scott D. Peckham <Scott.Peckham@colorado.edu>
2019-10-07 17:23:02 -0400

    Tested and fixed bugs.

--- topoflow/utils/init_depth.py
+++ topoflow/utils/init_depth.py
@@ -6,6 +6,7 @@
 #------------------------------------------------------------------------
 
 #  test1()
+#  test2()
 
 #  remove_bad_slopes()
 #  compute_initial_depth()
@@ -26,6 +27,52 @@ def test1():
     compute_initial_depth()
 
 #   test1()
+
+#------------------------------------------------------------------------
+def test2():
+
+    site_prefix = 'Baro_Gam_1min'
+    cfg_dir = '/Users/peckhams/Desktop/TopoFlow_2019/__Regions/'
+    cfg_dir += 'Ethiopia/DEMs/MERIT/Baro_Gam_1min/'
+    #--------------------------------------------------------
+    area_file    = (site_prefix + '_area.rtg')
+    slope_file   = (site_prefix + '_slope.rtg')
+    width_file   = (site_prefix + '_chan-w.rtg') 
+    manning_file = ( site_prefix + '_chan-n.rtg')
+    sinu_file    = (site_prefix + '_sinu.rtg')
+    # angle_file = (site_prefix + '_chan-a.rtg')
+    #--------------------------------------------------------
+    d0_file = (site_prefix + '_d0.rtg')
+
+    #--------------------------------------------    
+    # Estimate baseflow rate for the Baro River
+    #--------------------------------------------
+    A = np.float64(23567.7)    # [km2]
+    A *= 1e6    # [km2 -> m2]
+    u = 1.0        # [m s-1]
+    w = 140.0      # [m]
+    d = 6.0        # [m]
+    Q = u * w * d  # [m3 s-1]
+    B = Q / A      # [m s-1]    (units needed)
+    B = B / 6.0  ### (arbitrary reduction factor)
+    #-------------------------------------------------------
+    # print('Baseflow estimate = ' + str(B) + ' [m s-1]' )
+    B2 = B * 1000.0 * 3600.0   # [mm h-1] 
+    print('Baseflow estimate = ' + str(B2) + ' [mm h-1]' )
+
+
+    #----------------------------------------------     
+    # Compute the initial channel flow depth grid
+    #----------------------------------------------                                          
+    compute_initial_depth( site_prefix=site_prefix, cfg_dir=cfg_dir,
+            baseflow_rate=B, tol=None, SILENT=False,
+            #--------------------------------------------
+            area_file=area_file, slope_file=slope_file,
+            width_file=width_file, manning_file=manning_file,
+            sinu_file=sinu_file, angle_file=None,
+            d0_file=d0_file)
+ 
+#   test2()                          
 #------------------------------------------------------------------------
 def remove_bad_slopes(slope, FLOAT=False, SILENT=False):
 
@@ -103,8 +150,9 @@ def compute_initial_depth( site_prefix=None, cfg_dir=None,
                            SILENT=False,
                            #-----------------------------------
                            area_file=None, slope_file=None,
-                           width_file=None, angle_file=None,
-                           manning_file=None, d0_file=None):
+                           width_file=None, manning_file=None,
+                           sinu_file=None, angle_file=None,
+                           d0_file=None, v0_file=None):
 
     #------------------------------------------------------------
     # Note:  This routine uses a grid-based Newton-Raphson
@@ -169,7 +217,7 @@ def compute_initial_depth( site_prefix=None, cfg_dir=None,
     # output:  d0_file (and optional v0_file)
     #----------------------------------------------
     header_file = (cfg_dir + site_prefix + '.rti')
-    grid_info   = rti_files.read_info( header_file, REPORT=True)
+    grid_info   = rti_files.read_info( header_file, REPORT=False)
     byte_order  = grid_info.byte_order
     ncols       = grid_info.ncols
     nrows       = grid_info.nrows
@@ -180,15 +228,28 @@ def compute_initial_depth( site_prefix=None, cfg_dir=None,
     A_file = cfg_dir + area_file
     S_file = cfg_dir + slope_file
     w_file = cfg_dir + width_file
-    q_file = cfg_dir + angle_file    # q = theta = bank_angle
     n_file = cfg_dir + manning_file
     #---------------------------------------------------------------
+    # Note: S, w and n may have NaNs on edges.  A has zeros.
+    #---------------------------------------------------------------    
     A = rtg_files.read_grid( A_file, grid_info, RTG_type='FLOAT' )
     S = rtg_files.read_grid( S_file, grid_info, RTG_type='FLOAT' )
     w = rtg_files.read_grid( w_file, grid_info, RTG_type='FLOAT' )
-    q = rtg_files.read_grid( q_file, grid_info, RTG_type='FLOAT' )
     n = rtg_files.read_grid( n_file, grid_info, RTG_type='FLOAT' )
-
+    #------------------------------------------------------------------------   
+    if (angle_file is not None):
+        q_file = cfg_dir + angle_file    # q = theta = bank_angle
+        theta = rtg_files.read_grid( q_file, grid_info, RTG_type='FLOAT' )
+    else:
+        theta = 30.0   # [degrees]
+    #------------------------------------------------------------------------  
+    if (sinu_file is not None):
+        sinu_file = cfg_dir + sinu_file  
+        sinu = rtg_files.read_grid( sinu_file, grid_info, RTG_type='FLOAT' )
+        sinu[ sinu == 0 ] = 1.0  # (starts with zeros on edges)
+    else:
+        sinu = 1.0
+             
     #---------------------------------
     # Convert A from [km^2] to [m^2]
     #---------------------------------
@@ -197,7 +258,7 @@ def compute_initial_depth( site_prefix=None, cfg_dir=None,
     #------------------------------------
     # Convert theta from [deg] to [rad]
     #------------------------------------
-    q = q * (np.pi / np.float64(180))    # [radians]
+    theta = theta * (np.pi / np.float64(180))    # [radians]
 
     #--------------------------------------
     # Remove zero slopes from slope grid
@@ -207,29 +268,36 @@ def compute_initial_depth( site_prefix=None, cfg_dir=None,
     # then zero slopes should be gone.
     #--------------------------------------
     S = remove_bad_slopes( S )
+
+    #---------------------------------    
+    # Adjust slopes by the sinuosity
+    #---------------------------------
+    S = (S / sinu)
     
-    ;------------------------------------------------
+    #------------------------------------------------
     # Option 1:
     # Initialize to very large depth so that we can
     # get largest root (and smallest velocity) if
     # there are multiple roots.
-    ;------------------------------------------------
-    d0 = np.float64( 500 )    # [meters]
-    d  = np.zeros( [nrows, ncols], dtype='float64') + d0
+    #------------------------------------------------
+    # Initialize d, to be saved to d0_file
+    #------------------------------------------------    
+    d1 = np.float64( 500 )    # [meters]
+    d  = np.zeros( [nrows, ncols], dtype='float64') + d1
 
     #------------------------------------------------
     # Option 2:
     # Initialize depth grid, d, for Newton iteration
     # by assuming the case where d << w and R = d,
     # where R = Ac/P = hydraulic radius.
-    ;-------------------------------------------------
+    #-------------------------------------------------
     ## p1 = np.float64(3) / 5
     ## d = K / w**p1
         
     #-----------------------------------------
     # Construct the constant K, where:
     #    F(d) = Ac^(5/3) - [K * P^(2/3)] = 0
-    ;-----------------------------------------
+    #-----------------------------------------
     K = (B * A * n) / np.sqrt(S)
 
     #------------------
@@ -246,14 +314,15 @@ def compute_initial_depth( site_prefix=None, cfg_dir=None,
     # Iterate entire grid to get d, using a
     # grid-based Newton root-finding method
     #----------------------------------------
+    print('Iterating...')
     DONE = False
     while not(DONE):
         last_d = d.copy()
         Ac     = d * (w + (d * np.tan(theta)))
         P      = w + (2 * d / np.cos(theta))
-        numer  = Ac**pow1 - (K * P**pow2)
-        term1  = pow1 * Ac^pow2 * (w + (2*d*np.tan(theta)))
-        term2  = pow2 * K * P**pow3 * (2 / np.cos(theta))
+        numer  = (Ac**pow1) - (K * P**pow2)
+        term1  = pow1 * (Ac**pow2) * (w + (2*d*np.tan(theta)))
+        term2  = pow2 * K * (P**pow3) * (2 / np.cos(theta))
         denom  = term1 - term2
         d      = d - (numer/denom)
 
@@ -271,19 +340,21 @@ def compute_initial_depth( site_prefix=None, cfg_dir=None,
         ### gmax = gap.max()  ### HOW TO EXCLUDE NANs?
         ### print('     gmin, gmax = ' + str(gmin) + ', ' + str(gmax) )
 
-        #-----------==-------
+        #--------------------
         # Are we done yet ?
         #--------------------
         n_tries += 1
-        wb = np.where( gap > tol )
-        nb = np.size( wb[0] )
+        wb = (gap > tol)  # (array of True or False)
+        nb = wb.sum()
+        ## wb = np.where( gap > tol )
+        ## nb = np.size( wb[0] )
         DONE = (nb == 0) or (n_tries > max_tries)
         
         #------------------------------
         # Write status to message box
         #------------------------------
         if not(SILENT):
-            mstr = 'Pixels left = ' + str(nwg)
+            mstr = 'Pixels left = ' + str(nb)
             print( mstr )
     
         #----------------------------------
@@ -315,10 +386,17 @@ def compute_initial_depth( site_prefix=None, cfg_dir=None,
     # on byte order recorded in grid_info object.
     #------------------------------------------------
     d0_file2 = (cfg_dir + d0_file)
-    rtg_files.write_grid( d0, d0_file2, grid_info, RTG_type='FLOAT')
+    rtg_files.write_grid( d, d0_file2, grid_info, RTG_type='FLOAT')
     print( 'Finished writing file: ')
     print( d0_file2 )
-    
+
+    #---------------------------------    
+    # Print min and max values of d0
+    #---------------------------------
+    if not(SILENT):
+        print( 'd_min = ' + str(np.nanmin(d)) + ' [m]')
+        print( 'd_max = ' + str(np.nanmax(d)) + ' [m]')
+
     #------------------------------------
     # Compute the initial velocity grid
     # using the Manning formula ?
