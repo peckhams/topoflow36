9cdff7f978f3ae868289563907b8c5f7faec7ee8
Scott D. Peckham <Scott.Peckham@colorado.edu>
2024-07-05 14:38:41 -0600

    Add files via upload

--- topoflow/main2.py
+++ topoflow/main2.py
@@ -17,10 +17,20 @@ class tf36_bmi( multi_bmi.multi_bmi ):
     pass
 
 #-----------------------------------------------------------------------
-def run_model( cfg_file=None, cat_id_str='cat-84', SILENT=False ):
+def run_model( cfg_file=None, cat_id_str='cat-209',
+               ngen_dir=None, SILENT=False ):
 
+    if (ngen_dir is None):
+        print('========================================================')
+        print('ERROR: You must specify the full path to the directory')
+        print('where you have installed NextGen, using the ngen_dir')
+        print('keyword:  e.g., ngen_dir="~/ngen".')
+        print('========================================================')
+        print()
+        return
     tf = tf36_bmi( SILENT=SILENT )
-
+    tf.ngen_dir = ngen_dir
+    
     if (cfg_file is None):
         if (cat_id_str is not None):
             cfg_file = tf.get_test_cfg_file( test=cat_id_str)
